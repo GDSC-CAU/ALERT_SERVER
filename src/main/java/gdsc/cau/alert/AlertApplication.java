19aa22052d017271be188504bad3ed7712f9da11
CHAE <chaso1495@naver.com>
2024-01-30 16:52:04 +0900

    [feat] 타임스탬프용 추상클래스 선언 (#1)

--- src/main/java/gdsc/cau/alert/AlertApplication.java
+++ src/main/java/gdsc/cau/alert/AlertApplication.java
@@ -2,7 +2,9 @@ package gdsc.cau.alert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class AlertApplication {
 
---
+++ src/main/java/gdsc/cau/alert/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package gdsc.cau.alert.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
