7f0d4c39300e736be60f6a36a6df85b1a41d552f
CHAE <chaso1495@naver.com>
2024-01-30 18:25:21 +0900

    [chore] 응답 코드 보관용 ResponseCode 추가 (#3)

---
+++ src/main/java/gdsc/cau/alert/util/api/ResponseCode.java
@@ -0,0 +1,37 @@
+package gdsc.cau.alert.util.api;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public enum ResponseCode {
+
+    // 400 Bad Request
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, false, "잘못된 요청입니다."),
+
+    // 401 Unauthorized
+
+    // 403 Forbidden
+    FORBIDDEN(HttpStatus.FORBIDDEN, false, "권한이 없습니다."),
+
+    // 404 Not Found
+
+    // 405 Method Not Allowed
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, false, "허용되지 않은 메소드입니다."),
+
+    // 409 Conflict
+
+    // 500 Internal Server Error
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, false, "서버에 오류가 발생하였습니다.");
+
+    // 200 OK
+
+    // 201 Created
+
+    private final HttpStatus httpStatus;
+    private final Boolean success;
+    private final String message;
+}
