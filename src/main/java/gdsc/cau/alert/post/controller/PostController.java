88bd03294e9c1d9b2dbc78852e3171904fdc8485
CHAE <chaso1495@naver.com>
2024-02-12 23:28:18 +0900

    [chore] CI/CD 적용 (#11)

--- src/main/java/gdsc/cau/alert/post/controller/PostController.java
+++ src/main/java/gdsc/cau/alert/post/controller/PostController.java
@@ -3,7 +3,6 @@ package gdsc.cau.alert.post.controller;
 import gdsc.cau.alert.util.api.ApiResponse;
 import gdsc.cau.alert.util.api.ResponseCode;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
